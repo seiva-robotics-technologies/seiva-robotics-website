cb4ce50cd15ea9cb9b576550e2c65e312e6e2bb3
SEIVA ROBOTICS & TECHNOLOGIES <seivaroboticstechnologies@gmail.com>
2026-03-04 01:44:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
+alert("Thank you for contacting Seiva Robotic & Technologies. Our team will reach out to you shortly.");
+});
